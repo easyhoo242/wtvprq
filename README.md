86d6f695b42eafe9c286f20e93303d9f31230cf2
easyhoo242 <88233704+easyhoo242@users.noreply.github.com>
2023-05-24 07:03:27 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# wtvprq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/wtvprq)
---
+++ TreeComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TreeComponent from './TreeComponent.vue';
+
+interface TreeData {
+  key: string;
+  title: string;
+  children: TreeData[];
+}
+defineProps<{ data: TreeData[] }>();
+</script>
+
+<template>
+  <!-- do something.... -->
+  <div v-for="item in data" :key="item.key">
+    <header>{{ item.title }}</header>
+
+    <section v-if="item.children">
+      <TreeComponent :data="item.children" />
+    </section>
+  </div>
+</template>
